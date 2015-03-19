5c57d1a3ef286f5f141cc7d803a759fce91691e4
moznion <moznion@gmail.com>
2015-03-19 14:33:02 +0900

    Initial commit

---
+++ src/test/java/net/moznion/random/string/RandomStringGeneratorTest.java
@@ -0,0 +1,15 @@
+package net.moznion.random.string;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	@Test
+	public void shouldAlwaysPass() {
+		assertTrue(true);
+	}
+}
+
