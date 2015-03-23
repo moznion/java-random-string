299c9bbaa532416a1d9863675b74fc74947f8328
moznion <moznion@gmail.com>
2015-03-23 18:29:33 +0900

    Extract regex normalizer to own class

--- src/main/java/net/moznion/random/string/RandomStringGenerator.java
+++ src/main/java/net/moznion/random/string/RandomStringGenerator.java
@@ -6,8 +6,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 /**
@@ -168,7 +166,7 @@ public class RandomStringGenerator {
    * @return Random String
    */
   public String generateByRegex(final String regex) {
-    String expanded = normalizeQuantifiers(regex);
+    String expanded = new RegexNormalizer(numOfUpperLimit, random).normalizeQuantifiers(regex);
 
     final String[] regexCharacters = expanded.split("");
     final int length = regexCharacters.length;
@@ -288,65 +286,5 @@ public class RandomStringGenerator {
    */
   public void setNumOfUpperLimit(int numOfUpperLimit) {
     this.numOfUpperLimit = numOfUpperLimit;
-  }
-
-  // for repetition quantifier, e.g. {1,4}
-  private static final Pattern REPETITION_QUANTIFIER_RE =
-      Pattern.compile("([^\\\\])\\{([0-9]+),([0-9]+)?\\}");
-  private static final Pattern ASTERISK_QUANTIFIER_RE = Pattern.compile("([^\\\\])\\*");
-  private static final Pattern PLUS_QUANTIFIER_RE = Pattern.compile("([^\\\\])\\+");
-  private static final Pattern QUESTION_QUANTIFIER_RE = Pattern.compile("([^\\\\])\\?");
-
-  private String normalizeQuantifiers(final String regex) {
-    String expanded = regex;
-
-    Matcher repetitionMatcher = REPETITION_QUANTIFIER_RE.matcher(expanded);
-    while (repetitionMatcher.find()) {
-      int start = Integer.parseInt(repetitionMatcher.group(2), 10);
-      int end;
-      if (repetitionMatcher.group(3) == null) {
-        end = numOfUpperLimit;
-      } else {
-        end = Integer.parseInt(repetitionMatcher.group(3), 10);
-      }
-      expanded =
-          repetitionMatcher.replaceFirst(repetitionMatcher.group(1) + "{"
-              + getRandomNumAsString(start, end) + "}");
-      repetitionMatcher = REPETITION_QUANTIFIER_RE.matcher(expanded);
-    }
-
-    Matcher asteriskMatcher = ASTERISK_QUANTIFIER_RE.matcher(expanded);
-    while (asteriskMatcher.find()) {
-      expanded =
-          asteriskMatcher.replaceFirst(asteriskMatcher.group(1) + "{"
-              + getRandomNumAsString(0, numOfUpperLimit) + "}");
-      asteriskMatcher = ASTERISK_QUANTIFIER_RE.matcher(expanded);
-    }
-    //
-    Matcher plusMatcher = PLUS_QUANTIFIER_RE.matcher(expanded);
-    while (plusMatcher.find()) {
-      expanded =
-          plusMatcher.replaceFirst(plusMatcher.group(1) + "{"
-              + getRandomNumAsString(1, numOfUpperLimit) + "}");
-      plusMatcher = PLUS_QUANTIFIER_RE.matcher(expanded);
-    }
-
-    Matcher questionMatcher = QUESTION_QUANTIFIER_RE.matcher(expanded);
-    while (questionMatcher.find()) {
-      expanded =
-          questionMatcher.replaceFirst(questionMatcher.group(1) + "{"
-              + getRandomNumAsString(0, 1) + "}");
-      questionMatcher = QUESTION_QUANTIFIER_RE.matcher(expanded);
-    }
-
-    return expanded;
-  }
-
-  private String getRandomNumAsString(final int start, final int end) {
-    int bound = end - start;
-    if (bound < 0) {
-      throw new RuntimeException("Detected invalid quantifier: " + "{" + start + "," + end + "}");
-    }
-    return Integer.toString(random.nextInt(bound + 1) + start, 10);
   }
 }
